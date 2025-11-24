a24dea3b633fc5cf34f31cd88f57608502bac639
amiral-fu <amiral-fu@mail.com>
2025-11-24 18:31:09 +0100

    [+] Add some fix

--- build-transfert.sh
+++
@@ -1,3 +0,0 @@
-docker build -t veec-comm-generator:`cat VERSION.md` -f Dockerfile.prod .
-docker save veec-comm-generator:`cat VERSION.md` -o /tmp/veec-comm-generator_`cat VERSION.md`.docker
-scp -P 2222 /tmp/veec-comm-generator_`cat VERSION.md`.docker furyfly.fr:/tmp/
